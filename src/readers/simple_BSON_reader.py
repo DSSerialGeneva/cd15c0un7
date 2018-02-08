55f50eeb185907a84d24d73bdef718fa051b6ee3
kisscool62 <kisscool62@gmail.com>
2018-02-08 20:05:46 +0100

    added pca and dataframe use instead of numpy

--- src/readers/simple_BSON_reader.py
+++ src/readers/simple_BSON_reader.py
@@ -1,12 +1,13 @@
 import csv
 import hashlib
 import io
-import operator
 import os
 
 import bson
 import numpy as np
 import pandas as pd
+
+from sklearn.decomposition import pca
 from PIL import Image
 
 from src.domain.img_meta import ImgMeta
@@ -20,7 +21,9 @@ class ParsingResult:
     pixel_matrix = np.empty(())
     categories = dict()  # to list and count the categories
     products = dict()  # to list and count the products
-    pictures_categories = np.empty(())  # to list and count the couples (picture hash, category)
+
+    # to list and count the couples (picture hash, category)
+    pictures_categories = pd.DataFrame(columns=['category', 'pic_hash', 'count'])
     pictures = dict()  # to list and count the pictures hashes
     twin_pictures = dict()  # to list and count the pictures which are twice with a different categories
 
@@ -46,7 +49,7 @@ class SimpleBSONReader:
         counters[key] = counters.get(key, 0) + 1
 
     @staticmethod
-    def read_all(file_name, save_intermediate=False):
+    def read_all(file_name, save_intermediate=False, save_png=True, pca_reduction=True):
 
         if save_intermediate:
             if not os.path.exists(ROOT_DIR):
@@ -73,28 +76,42 @@ class SimpleBSONReader:
 
                 img_meta = ImgMeta(product_id, category_id, no_pic)
 
-                flatten_ = img_meta.__array__() + tuple(Img2ArrayGray.transform(picture, True, img_meta).flatten())
+                img_pixel_matrix = Img2ArrayGray.transform(picture, save_png, img_meta)
+                if pca_reduction:
+                    # perform pca reduction
+                    print("Performing pca reduction of the current img (%s, %s, %s)" % (product_id, category_id, no_pic))
+                    img_pixel_matrix_reduced = pca.PCA(n_components=90, ).fit_transform(img_pixel_matrix)
+                    flatten_img_reduced = img_meta.__array__() + tuple(img_pixel_matrix_reduced.flatten())
+                flatten_img = img_meta.__array__() + tuple(img_pixel_matrix.flatten())
                 if parsing_result.pixel_matrix.shape == ():
-                    parsing_result.pixel_matrix = np.array(flatten_)
+                    parsing_result.pixel_matrix = np.array(flatten_img)
                 else:
                     parsing_result.pixel_matrix = np.vstack((
                         parsing_result.pixel_matrix,
-                        flatten_
+                        flatten_img
                     ))
             if save_intermediate:
                 f = open(ROOT_DIR + "%s-%s.csv" % (category_id, product_id), 'w')
-                f.write(",".join(map(str, flatten_)))
+                f.write(",".join(map(str, flatten_img)))
+                if pca_reduction:
+                    f_red = open(ROOT_DIR + "reduced/pca_%s-%s.csv" % (category_id, product_id), 'w')
+                    f_red.write(",".join(map(str, flatten_img_reduced)))
+
         if save_intermediate:
             with open(ROOT_DIR + "total.csv", 'w') as f:
                 f.write("%i" % c)
             SimpleBSONReader.write_dict_to_file("categories.csv", parsing_result.categories)
             SimpleBSONReader.write_dict_to_file("products.csv", parsing_result.products)
             SimpleBSONReader.write_dict_to_file("pictures.csv", parsing_result.pictures)
+            SimpleBSONReader.write_dict_to_file("twin_pictures.csv", parsing_result.twin_pictures)
 
-            df = pd.DataFrame(parsing_result.pictures_categories)
-            SimpleBSONReader.write_df_to_file("pictures_categories.csv", df)
-            df = df[df[2].map(int) != FIRST_OCCURENCE_NUMBER]
-            SimpleBSONReader.write_df_to_file("pictures_categories_cleaned.csv", df)
+            SimpleBSONReader.write_df_to_file("pictures_categories.csv", parsing_result.pictures_categories)
+
+            # clean picture_categories means keep only same (picture, category) couple
+            pictures_categories_cleaned = parsing_result.pictures_categories[
+                parsing_result.pictures_categories['count'] != FIRST_OCCURENCE_NUMBER
+            ]
+            SimpleBSONReader.write_df_to_file("pictures_categories_cleaned.csv", pictures_categories_cleaned)
         return parsing_result.pixel_matrix
 
     @staticmethod
@@ -113,44 +130,26 @@ class SimpleBSONReader:
         md5.update(pic['picture'])
         md5_strip = md5.hexdigest().strip()
         category_id = str(category_id)
-        pict_cat_row = [category_id, md5_strip, FIRST_OCCURENCE_NUMBER]
-        if parsing_result.pictures_categories.shape == ():
-            is_new_category = False  # because we don't want to mark the category as new when the picture is new too
-            if parsing_result.pictures_categories.shape == ():
-                parsing_result.pictures_categories = np.array(pict_cat_row)
-            else:
-                parsing_result.pictures_categories = np.vstack((parsing_result.pictures_categories, pict_cat_row))
+        index = category_id.__str__() + md5_strip
+
+        if index in parsing_result.pictures_categories.index:
+            parsing_result.pictures_categories.loc[index] = \
+                [category_id, md5_strip, parsing_result.pictures_categories.loc[index]['count'] + 1]
         else:
-            if parsing_result.pictures_categories.shape == (3,):
-                # first row
-                same_pictures = np.nonzero(parsing_result.pictures_categories[1] == md5_strip)[0]
-            else:
-                # other rows
-                same_pictures = np.nonzero(parsing_result.pictures_categories[:, 1] == md5_strip)[0]
-            not_new_picture = len(same_pictures) > 0
-            is_new_category = True
-            if is_new_category and not_new_picture:
-                # we'll find if it's a new category for the existing same pictures
-                for i_same_picture in same_pictures:
-                    same_category = parsing_result.pictures_categories[i_same_picture, 0] == category_id
-                    if same_category:
-                        # when category is the same, increase the counter, set new category to false
-                        is_new_category = False
-                        parsing_result.pictures_categories[i_same_picture, 2] = 1 + int(parsing_result.pictures_categories[i_same_picture, 2])
-                        break
-            else:
-                is_new_category = False  # because we don't want to mark the category as new when the picture is new too
-                if parsing_result.pictures_categories.shape == ():
-                    parsing_result.pictures_categories = np.array(pict_cat_row)
-                else:
-                    parsing_result.pictures_categories = np.vstack((parsing_result.pictures_categories, pict_cat_row))
-        if is_new_category:
-            SimpleBSONReader.increment_counter_dict(parsing_result.twin_pictures, md5_strip)
+            parsing_result.pictures_categories.loc[index] = [category_id, md5_strip, FIRST_OCCURENCE_NUMBER]
+            if sum(parsing_result.pictures_categories['pic_hash'] == md5_strip) > 1:
+                SimpleBSONReader.increment_counter_dict(parsing_result.twin_pictures, md5_strip)
+
         SimpleBSONReader.increment_counter_dict(parsing_result.pictures, md5_strip)
 
     @staticmethod
+    def add_pic_categ_row(index, parsing_result, pict_cat_row):
+        parsing_result.pictures_categories.loc[index] = pict_cat_row
+
+    @staticmethod
     def write_csv_dictionary(csv_file, dictionary):
-        csv.writer(csv_file).writerows(sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True))
+        csv.writer(csv_file).writerows(dictionary.items())
+        # sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True))
 
     @staticmethod
     def write_csv_df(csv_file, df):
